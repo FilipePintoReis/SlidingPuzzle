fab038f2bdc8c4b4db1eadd5fdb2836d80f1621c
Filipe Pinto Reis <filipepintodosreis@gmail.com>
2020-09-04 11:32:42 +0100

    starting piece path

--- Board/PathMethods.py
+++ Board/PathMethods.py
@@ -1,8 +1,13 @@
 def zero_path(self, path, piece): # TODO 
     pass
 
-def piece_path(self, piece): # TODO 
-    pass
+def piece_path(self, piece, piece_type): # TODO 
+    if piece_type == 
+    if self.check_if_penult(piece):
+        self.penult_piece_to_ult_posis(piece)
+    elif self.check_if_ult(piece):
+        self.ult_piece_near_penult(piece)
+
 
 def penult_piece_to_ult_posis(self, piece): # TODO 
     pass
